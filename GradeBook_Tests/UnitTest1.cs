0123ba66f0ab6290b4fb112a3d2b0a89dae4a670
Jacob Fish <jacobfish@quickenloans.com>
2020-09-22 15:32:52 -0400

    recommiting initial push

---
+++ GradeBook_Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using GradeBook;
+using GradeBook.Models.Interfaces;
+using GradeBook.Models;
+using Moq;
+
+namespace GradeBook_Tests
+{
+    public class UnitTest1
+    {
+
+
+        [Fact]
+        public void Test1()
+        {
+        
+        }
+    }
+}
